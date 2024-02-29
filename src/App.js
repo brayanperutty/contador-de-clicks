f922ed2c7b6ee3b06fbc797ed9a941bc1a7b2576
brayanperutty <brayanperutty@gmail.com>
2024-02-29 17:29:35 -0500

    Se finaliza el ejercicio de contador de clics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import './App.css';
+import Boton from './components/boton';
+import Contador from './components/contador';
 import freeCodeCampLogo from './img/fcc_secondary_small.svg';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setNumClics] =  useState(0);
+
+  const manejaClic = () => {
+    setNumClics(numClics + 1);  
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
       <div className='freecodecamp-logo-contenedor'>
         <img 
           className='freecodecamp-logo' 
           src={freeCodeCampLogo} 
-          alt='Logo freeCodeCamp'/>
+          alt='Logo freeCodeCamp'
+        />
       </div>
       <div className='contenedor-principal'>
-          
+        <Contador 
+          numClics={numClics} 
+        />
+        <Boton
+          text='Clic'
+          esBotonDeClic={true}
+          manejaClic={manejaClic}
+        />
+        <Boton
+          text='Reiniciar'
+          esBotonDeClic={false}
+          manejaClic={reiniciarContador}
+        />
       </div>
     </div>
   );
--- src/components/boton.js
+++ src/components/boton.js
@@ -1,10 +1,15 @@
 import React from "react";
+import '../styles/boton.css';
 
-function Boton({ text, esBotonDeClic }){
+function Boton({ text, esBotonDeClic, manejaClic }){
   return(
     <button
-      className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }>
+      className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
+      onClick={manejaClic}  
+    >
       {text}
     </button>
   );
 }
+
+export default Boton;
